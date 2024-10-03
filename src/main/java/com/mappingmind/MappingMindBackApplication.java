adb1c9e063fadd1da778f51152e36227a8e05079
Carlos Oliveira <ramalhogithub@gmail.com>
2024-10-03 11:32:26 -0300

    Initial commit

---
+++ src/main/java/com/mappingmind/MappingMindBackApplication.java
@@ -0,0 +1,13 @@
+package com.mappingmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingMindBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingMindBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mappingmind/MappingMindBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mappingmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingMindBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
